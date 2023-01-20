274b4e88de158899197b70b73028381f13a19e01
Alexander Gavrilyuk <one1970zero@mail.ru>
2023-01-20 15:17:52 +0300

    Изучение Kotlin

---
+++ src/main/kotlin/Game.kt
@@ -0,0 +1,36 @@
+fun main(args: Array<String>) {
+    val name = "Madrigal"
+    var healthPoints = 89
+    val isBlessed = true
+    val isImmortal = false
+
+    //Aura
+    val  auraVisible = isBlessed && healthPoints > 50 || isImmortal
+    val  auraColor = if (auraVisible) "GREEN" else "NONE"
+    println(auraColor)
+
+    val healthStatus = when (healthPoints){
+        100 -> " is in excellent condition!"
+        in  90..99  -> " has a few scratches"
+        in 75..89 -> if (isBlessed){
+            " has some minor wounds but is healing quite quickly!"
+        }else{
+            " has some minor wounds"
+        }
+        in 15..74 -> " looks pretty hurt"
+        else -> " is in awful condition!"
+    }
+
+    //Состояние игрока
+    println(name + " " + healthStatus)
+
+    //race
+    val race = "gnome"
+    val faction = when (race){
+        "dwarf" -> "Keepers of the Mines"
+        "gnome" -> "Keepers of the Mines"
+        "orc" -> "Free People of the Rolling Hills"
+        "human" -> "Free People of the Rolling Hills"
+        else -> {}
+    }
+}
